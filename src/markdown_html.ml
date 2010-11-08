4a3bf4c1b17915492919d4739c8146c59f0eee05
Anil Madhavapeddy <anil@recoil.org>
2010-11-08 16:54:05 +0000

    Markdown: quote all text into entities, and wrap paragraphs in a div

--- src/markdown_html.ml
+++ src/markdown_html.ml
@@ -3,19 +3,31 @@ open Markdown
 
 let quote str = "\"" ^ str ^ "\""
 
+let escape ibuf =
+  let obuf = Buffer.create (String.length ibuf) in
+  let add c = Buffer.add_string obuf c in 
+  for i = 0 to String.length ibuf - 1 do
+    match ibuf.[i] with
+    |'<' -> add "&lt;"
+    |'>' -> add "&gt;"
+    |'&' -> add "&amp;"
+    |c -> Buffer.add_char obuf c
+  done;
+  Buffer.contents obuf
+
 let rec text = function
-    Text t    -> <:html< $str:t$ >>
-  | Emph t    -> <:html< <i>$str:t$</> >>
-  | Bold t    -> <:html< <b>$str:t$</> >>
+    Text t    -> <:html< $str:escape t$ >>
+  | Emph t    -> <:html< <i>$str:escape t$</> >>
+  | Bold t    -> <:html< <b>$str:escape t$</> >>
   | Struck pt -> <:html< <del>$par_text pt$</> >>
-  | Code t    -> <:html< <code>$str:t$ </> >>
-  | Link href -> <:html< <a href=$str:quote href.href_target$>$str:href.href_desc$</> >>
+  | Code t    -> <:html< <code>$str:escape t$ </> >>
+  | Link href -> <:html< <a href=$str:quote href.href_target$>$str:escape href.href_desc$</> >>
   | Anchor a  -> <:html< <a name=$str:quote a$/> >>
   | Image img -> <:html< <img src=$str:quote img.img_src$ alt=$str:quote img.img_alt$/> >>
 
 and para = function
     Normal pt        -> <:html< $par_text pt$ >>
-  | Pre (t,kind)     -> <:html< <pre><code>$str:t$</></> >>
+  | Pre (t,kind)     -> <:html< <pre><code>$str:escape t$</></> >>
   | Heading (1,pt)   -> <:html< <h1>$par_text pt$</> >>
   | Heading (2,pt)   -> <:html< <h2>$par_text pt$</> >>
   | Heading (3,pt)   -> <:html< <h3>$par_text pt$</> >>
@@ -32,6 +44,6 @@ and li pl =
 
 and paras ps =
   let aux p = <:html< <p>$para p$</> >> in
-  <:html< $list:List.map aux  ps$ >>
+  <:html< <div class="post"> $list:List.map aux  ps$ </> >>
 
 let t = paras
