bcc2513dfbec25dac8cbb85d20d4599eb0e64c97
Anil Madhavapeddy <anil@recoil.org>
2011-09-19 15:11:22 +0900

    fixes to build_direct_fs (now works!)

--- src/build_direct_fs.sh
+++ src/build_direct_fs.sh
@@ -7,5 +7,6 @@ dd if=/dev/zero of=../files.img bs=1024 count=8192
 dd if=/dev/zero of=../tmpl.img bs=1024 count=8192
 mir-fs-create ../files ../files.img
 mir-fs-create ../tmpl ../tmpl.img
+
 mir-build unix-direct/${BIN}.bin
-sudo ${MIR_RUN} -b unix-direct -vbd filesvbd:../files.img -vbd tmplvbd:../tmpl.img -kv_ro files:filesvbd -kv_ro tmpl:tmplvbd ./_build/unix-direct/${BIN}.bin
+sudo ${MIR_RUN} -b unix-direct -vbd staticvbd:../files.img -vbd templatesvbd:../tmpl.img -kv_ro static:staticvbd -kv_ro templates:templatesvbd ./_build/unix-direct/${BIN}.bin
