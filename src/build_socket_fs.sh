2491ff920ac3d56eb405d3e686647dea75b03b1d
Anil Madhavapeddy <anil@recoil.org>
2011-09-19 13:28:40 +0900

    remove strace left in debugging

--- src/build_socket_fs.sh
+++ src/build_socket_fs.sh
@@ -4,4 +4,4 @@ BIN=kv_ro_server
 MIR_RUN=$(which mir-run)
 
 mir-build unix-socket/${BIN}.bin
-sudo strace ${MIR_RUN} -b unix-socket -kv_ro static:../files -kv_ro templates:../tmpl ./_build/unix-socket/${BIN}.bin
+sudo ${MIR_RUN} -b unix-socket -kv_ro static:../files -kv_ro templates:../tmpl ./_build/unix-socket/${BIN}.bin
