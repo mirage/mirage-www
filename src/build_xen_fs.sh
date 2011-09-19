98f07ecc57ef0559caa6511b8d77fa60654797be
Anil Madhavapeddy <anil@recoil.org>
2011-09-19 09:49:42 +0100

    update scripts for xen

--- src/build_xen_fs.sh
+++ src/build_xen_fs.sh
@@ -8,10 +8,5 @@ dd if=/dev/zero of=../tmpl.img bs=1024 count=8192
 mir-fs-create ../files ../files.img
 mir-fs-create ../tmpl ../tmpl.img
 
-files_vbd=$(readlink -f ../files.img)
-tmpl_vbd=$(readlink -f ../tmpl.img)
-
-echo files=$files_vbd
-echo tmpl=$tmpl_vbd
 mir-build xen/${BIN}.xen
-sudo ${MIR_RUN} -b xen -vif xenbr0 -vbd ${files_vbd},hda1,r -vbd ${tmpl_vbd},hda2,r -kv_ro static:hda1 kv_ro templates:hda2 ./_build/xen/${BIN}.xen
+sudo ${MIR_RUN} -b xen -vif xenbr0 -vbd hda1:../files.img -vbd hda2:../tmpl.img -kv_ro static:hda1 kv_ro templates:hda2 ./_build/xen/${BIN}.xen
