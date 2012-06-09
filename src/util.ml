26187eb26420d813dcae41c033d7af3cf94a3c5e
Anil Madhavapeddy <anil@recoil.org>
2012-06-09 13:11:04 +0100

    use Cstruct instead of Bitstring

--- src/util.ml
+++ src/util.ml
@@ -2,4 +2,4 @@ open Lwt
 
 (* XXX very inefficient *)
 let string_of_stream s = 
-  Lwt_stream.to_list s >|= Bitstring.concat >|= Bitstring.string_of_bitstring
+  Lwt_stream.to_list s >|= Cstruct.copy_buffers
