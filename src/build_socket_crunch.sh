0ee54632584aa05742a2b0a25e060462d0ddc520
Anil Madhavapeddy <anil@recoil.org>
2012-06-25 22:43:43 +0100

    update build scripts to new scheme

--- src/build_socket_crunch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-BIN=crunch_server
-MIR_RUN=$(which mir-run)
-
-mir-crunch -name "static" ../files > filesystem_static.ml
-mir-crunch -name "templates" ../tmpl > filesystem_templates.ml
-mir-build unix-socket/${BIN}.bin
-sudo ${MIR_RUN} -b unix-socket ./_build/unix-socket/${BIN}.bin
--- src/build_xen_crunch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-BIN=crunch_server
-MIR_RUN=$(which mir-run)
-
-mir-crunch -name "static" ../files > filesystem_static.ml
-mir-crunch -name "templates" ../tmpl > filesystem_templates.ml
-mir-build xen/${BIN}.xen
-sudo ${MIR_RUN} -b xen -vif xenbr0 ./_build/xen/${BIN}.xen
