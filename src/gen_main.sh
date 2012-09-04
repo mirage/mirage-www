0ebcc8f498a3b1732a3af2a49ad70727c277d7bf
Anil Madhavapeddy <anil@recoil.org>
2012-09-03 19:17:14 -0700

    port to OASIS (Xen build broken currently)

---
+++ src/gen_main.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+mir-crunch -name "static" ../files > filesystem_static.ml
+mir-crunch -name "templates" ../tmpl > filesystem_templates.ml
+echo open Filesystem_static > main.ml
+echo open Filesystem_templates >> main.ml
+echo open Server >> main.ml
+echo "let _ = OS.Main.run (main ())" >> main.ml
