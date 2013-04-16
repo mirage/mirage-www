1bb917829f5c015e6184838745e6a0fa6e5c9756
Vincent Bernardoff <vincent.bernardoff@citrix.com>
2013-04-16 22:48:18 +0100

    Put info about people in a dedicated module to avoid repetition

--- src/wiki.ml
+++ src/wiki.ml
@@ -327,38 +327,7 @@ let page_css = <:css<
 
 (* Data *)
 
-let anil = {
-  Atom.name = "Anil Madhavapeddy";
-  uri       = Some "http://anil.recoil.org";
-  email     = Some "anil@recoil.org";
-}
-let thomas = {
-  Atom.name = "Thomas Gazagnaire";
-  uri       = Some "http://gazagnaire.org";
-  email     = Some "thomas@gazagnaire.org";
-}
-let raphael = {
-  Atom.name = "Raphael Proust";
-  uri       = Some "https://github.com/raphael-proust";
-  email     = Some "raphlalou@gmail.com";
-}
-let balraj = {
-  Atom.name = "Balraj Singh";
-  uri       = None;
-  email     = Some "balraj.singh@cl.cam.ac.uk";
-}
-let mort = {
-  Atom.name = "Richard Mortier";
-  uri       = Some "http://mort.io/";
-  email     = Some "mort@cantab.net";
-}
-let vb = {
-  Atom.name = "Vincent Bernardoff";
-  uri       = Some "https://github.com/vbmithr";
-  email     = Some "vb@luminar.eu.org";
-}
-
-let rights = Some "All rights reserved by the author"
+open People
 
 let categories = [
   "overview", [
