28605464697003a2a4a11661e5bc123f7f1a253d
Anil Madhavapeddy <anil@recoil.org>
2013-09-21 19:40:33 -0400

    add travis test scripts

---
+++ .travis-ci.sh
@@ -0,0 +1,15 @@
+# OPAM packages needed to build tests.
+OPAM_PACKAGES="mirage-unix mirage cow mirage-fs mirari cohttp"
+
+# Install OCaml and OPAM PPAs
+echo "yes" | sudo add-apt-repository ppa:avsm/ppa-testing
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+export OPAMYES=1
+export OPAMVERBOSE=1
+
+opam init 
+opam install ${OPAM_PACKAGES}
+
+eval `opam config -env`
+./default_build.sh
