87f577b52f480e5999ff37bb196633a707a6e638
Anil Madhavapeddy <anil@recoil.org>
2013-10-06 13:40:10 +0100

    Add support for retrieving a deployment key for mirage/mirage-www-deployment

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -8,8 +8,24 @@ sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
 export OPAMVERBOSE=1
 
-opam init 
-opam install ${OPAM_PACKAGES}
+opam init
+eval `opam config env`
 
-eval `opam config -env`
+# get the fake secure key out
+opam install travis-senv
+mkdir -p ~/.ssh
+SSH_DEPLOY_KEY=~/.ssh/id_dsa
+travis-senv decrypt > $SSH_DEPLOY_KEY
+chmod 600 $SSH_DEPLOY_KEY
+echo "Host mirdeploy github.com" >> ~/.ssh/config
+echo "   Hostname github.com" >> ~/.ssh/config
+echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+echo "   CheckHostIP no" >> ~/.ssh/config
+echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+git clone git@mirdeploy:mirage/mirage-www-deployment
+cd mirage-www-deployment
+cat README.md
+
+opam install ${OPAM_PACKAGES}
+cd $TRAVIS_BUILD_DIR
 ./default_build.sh
