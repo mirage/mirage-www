146d1cb1319606b3533f7ece7b82549233aec27c
Anil Madhavapeddy <anil@recoil.org>
2013-12-25 00:55:14 +0000

    Update site config to reflect real URL

--- src/site_config.ml
+++ src/site_config.ml
@@ -1,7 +1,7 @@
 open Data
 let blog read_entry = {
   (* TODO uri must be a fully qualified hostname *)
-  Cowabloga.Atom_feed.base_uri="/";
+  Cowabloga.Atom_feed.base_uri="http:///openmirage.org/";
   id = "";
   title = "The Mirage Blog";
   subtitle = Some "on building functional operating systems";
@@ -11,7 +11,7 @@ let blog read_entry = {
 }
 let wiki read_entry = {
   (* TODO uri must be a fully qualified hostname *)
-  Cowabloga.Atom_feed.base_uri="/";
+  Cowabloga.Atom_feed.base_uri="http://openmirage.org/";
   id = "";
   title = "The Mirage Documentation";
   subtitle = Some "guides and articles on using Mirage OS";
