1eec619e23a99920868dfed776cec16b165879fc
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-01-08 00:13:39 +0100

    Rewrite config.ml to use the new combinator API

--- src/config.ml
+++ src/config.ml
@@ -4,62 +4,33 @@ open Mirage
    MODE=crunch (or nothing): use static filesystem via crunch
    MODE=fat: use FAT and block device (run ./make-fat-images.sh)
  *)
-let static_fs =
-    Driver.KV_RO {
-    KV_RO.name = "files";
-    dirname    = "../files";
-  }
-
-let static_tmpl =
-  Driver.KV_RO {
-    KV_RO.name = "tmpl";
-    dirname    = "../tmpl";
-  }
-
-let fat_fs =
-  let block = {
-    Block.name = "fs_block";
-    filename   = "files.img";
-    read_only  = true;
-  } in
-  Driver.Fat_KV_RO {
-    Fat_KV_RO.name = "files";
-    block;
-  }
-
-let fat_tmpl =
-  let block = {
-    Block.name = "tmpl_block";
-    filename   = "tmpl.img";
-    read_only  = true;
-  } in
-  Driver.Fat_KV_RO {
-    Fat_KV_RO.name = "tmpl";
-    block;
-  }
-
-let http =
-  Driver.HTTP {
-    HTTP.port  = 80;
-    address    = None;
-    ip         = IP.local Network.Tap0;
-  }
-
 let mode =
-  try begin
-     match String.lowercase (Unix.getenv "MODE") with
-     | "fat" -> `Fat
-     | _ -> `Crunch
-  end with Not_found -> `Crunch
+  try match String.lowercase (Unix.getenv "FS") with
+    | "fat" -> `Fat
+    | _     -> `Crunch
+  with Not_found ->
+    `Crunch
 
-let drivers =
-  match mode with
-  | `Crunch -> [Driver.console; static_fs; static_tmpl; http]
-  | `Fat -> [Driver.console; fat_fs; fat_tmpl; http]
+let fat_ro file =
+  kv_ro_of_fs (fat (block_of_file file))
+
+let fs = match mode with
+  | `Fat    -> fat_ro "files.img"
+  | `Crunch -> crunch "../files"
+
+let tmpl = match mode with
+  | `Fat    -> fat_ro "tmpl.img"
+  | `Crunch -> crunch "../tmpl"
+
+let server = http_server 80 (default_ip [tap0])
+
+let main =
+  let libraries = [ "cow.syntax"; "cowabloga" ] in
+  let packages = [ "cow";"cowabloga" ] in
+  foreign ~libraries ~packages "Dispatch.Main"
+    (console @-> kv_ro @-> kv_ro @-> http @-> job)
 
 let () =
-  Mirage.add_to_opam_packages ["cow"; "cowabloga"];
-  Mirage.add_to_ocamlfind_libraries ["cow.syntax";"cowabloga"];
-  Job.register [
-    "Dispatch.Main", drivers
+  register "www" [
+    main $ default_console $ fs $ tmpl $ server
   ]
