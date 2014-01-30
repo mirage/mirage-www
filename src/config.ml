a54c2c7e0d46e180b0f442918b06f45edb992e81
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-01-30 12:31:00 +0000

    Update config.ml

--- src/config.ml
+++ src/config.ml
@@ -22,7 +22,28 @@ let tmpl = match mode with
   | `Fat    -> fat_ro "tmpl.img"
   | `Crunch -> crunch "../tmpl"
 
-let server = http_server 80 (default_ip [tap0])
+
+let net =
+  try match Sys.getenv "NET" with
+    | "direct" -> `Direct
+    | "socket" -> `Socket
+    | _        -> `Direct
+  with Not_found -> `Direct
+
+let dhcp =
+  try match Sys.getenv "DHCP" with
+    | "" -> false
+    | _  -> true
+  with Not_found -> false
+
+let stack console =
+  match net, dhcp with
+  | `Direct, true  -> direct_stackv4_with_dhcp console tap0
+  | `Direct, false -> direct_stackv4_with_default_ipv4 console tap0
+  | `Socket, _     -> socket_stackv4 console [Ipaddr.V4.any]
+
+let server =
+  http_server 80 (stack default_console)
 
 let main =
   let libraries = [ "cow.syntax"; "cowabloga" ] in
