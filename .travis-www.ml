be19ed45b5a2c73c9a13ce6b829679bc01df970b
Anil Madhavapeddy <anil@recoil.org>
2014-02-02 02:02:59 +0100

    Sync the static ipv4 config

--- .travis-www.ml
+++ .travis-www.ml
@@ -1,73 +1,63 @@
 open Mirage
 
+let ipv4_config =
+  let address = Ipaddr.V4.of_string_exn "128.232.97.54" in
+  let netmask = Ipaddr.V4.of_string_exn "255.255.255.224" in
+  let gateway = [Ipaddr.V4.of_string_exn "128.232.97.33"] in
+  { address; netmask; gateway }
+
 (* If the Unix `MODE` is set, the choice of configuration changes:
    MODE=crunch (or nothing): use static filesystem via crunch
    MODE=fat: use FAT and block device (run ./make-fat-images.sh)
  *)
-let static_fs =
-    Driver.KV_RO {
-    KV_RO.name = "files";
-    dirname    = "../files";
-  }
-
-let static_tmpl =
-  Driver.KV_RO {
-    KV_RO.name = "tmpl";
-    dirname    = "../tmpl";
-  }
-
-let fat_fs =
-  let block = {
-    Block.name = "fs_block";
-    filename   = "files.img";
-    read_only  = true;
-  } in
-  Driver.Fat_KV_RO {
-    Fat_KV_RO.name = "files";
-    block;
-  }
-
-let fat_tmpl =
-  let block = {
-    Block.name = "tmpl_block";
-    filename   = "tmpl.img";
-    read_only  = true;
-  } in
-  Driver.Fat_KV_RO {
-    Fat_KV_RO.name = "tmpl";
-    block;
-  }
-
-let http =
- let ip =
-    let open IP in
-    let address = Ipaddr.V4.of_string_exn "128.232.97.54" in
-    let netmask = Ipaddr.V4.of_string_exn "255.255.255.224" in
-    let gateway = [Ipaddr.V4.of_string_exn "128.232.97.33"] in
-    let config = IPv4 { address; netmask; gateway } in
-    { name = "www4"; config; networks = [ Network.Tap0 ] }
-  in
-  Driver.HTTP {
-    HTTP.port  = 80;
-    address    = None;
-    ip
-  }
-
 let mode =
-  try begin
-     match String.lowercase (Unix.getenv "MODE") with
-     | "fat" -> `Fat
-     | _ -> `Crunch
-  end with Not_found -> `Crunch
+  try match String.lowercase (Unix.getenv "FS") with
+    | "fat" -> `Fat
+    | _     -> `Crunch
+  with Not_found ->
+    `Crunch
 
-let drivers =
-  match mode with
-  | `Crunch -> [Driver.console; static_fs; static_tmpl; http]
-  | `Fat -> [Driver.console; fat_fs; fat_tmpl; http]
+let fat_ro dir =
+  kv_ro_of_fs (fat_of_files ~dir ())
+
+let fs = match mode with
+  | `Fat    -> fat_ro "files.img"
+  | `Crunch -> crunch "../files"
+
+let tmpl = match mode with
+  | `Fat    -> fat_ro "tmpl.img"
+  | `Crunch -> crunch "../tmpl"
+
+
+let net =
+  try match Sys.getenv "NET" with
+    | "direct" -> `Direct
+    | "socket" -> `Socket
+    | _        -> `Direct
+  with Not_found -> `Direct
+
+let dhcp =
+  try match Sys.getenv "DHCP" with
+    | "" -> false
+    | _  -> true
+  with Not_found -> false
+
+let stack console =
+  match net, dhcp with
+  | `Direct, true  -> direct_stackv4_with_dhcp console tap0
+  | `Direct, false -> direct_stackv4_with_static_ipv4 console tap0 ipv4_config
+  | `Socket, _     -> socket_stackv4 console [Ipaddr.V4.any]
+
+let server =
+  http_server 80 (stack default_console)
+
+let main =
+  let libraries = [ "cow.syntax"; "cowabloga" ] in
+  let packages = [ "cow";"cowabloga" ] in
+  foreign ~libraries ~packages "Dispatch.Main"
+    (console @-> kv_ro @-> kv_ro @-> http @-> job)
 
 let () =
-  Mirage.add_to_opam_packages ["cow"; "cowabloga"];
-  Mirage.add_to_ocamlfind_libraries ["cow.syntax";"cowabloga"];
-  Job.register [
-    "Dispatch.Main", drivers
+  register "www" [
+    main $ default_console $ fs $ tmpl $ server
   ]
