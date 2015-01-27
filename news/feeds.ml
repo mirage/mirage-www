6b574c7e69a8e81d1432ab5f3752e201e0d8fde7
manu <manu.delab@gmail.com>
2015-01-28 10:20:13 +1300

    remove non-mirage blogs from news feeds

--- news/feeds.ml
+++ news/feeds.ml
@@ -1,9 +1,7 @@
 let feeds = [
   ("Anil Madhavapeddy", "http://anil.recoil.org/feeds/atom.xml");
   ("Mindy Preston", "http://www.somerandomidiot.com/atom.xml");
-  ("Andrej Bauer", "http://math.andrej.com/feed/");
   ("Amir Chaudhry", "http://amirchaudhry.com/rss.xml");
-  ("Inria", "http://caml.inria.fr/hump.rss");
   ("My Little Garden of Code", "https://philtomson.github.io/atom.xml");
   ("Thomas Leonard", "http://roscidus.com/blog/atom.xml");
 ]
