d8ffcf61e80d9a0137e84264fa2293a22961c4db
manu <manu.delab@gmail.com>
2015-01-28 11:02:35 +1300

    Correcting Amir's feed

--- news/feeds.ml
+++ news/feeds.ml
@@ -1,7 +1,7 @@
 let feeds = [
   ("Anil Madhavapeddy", "http://anil.recoil.org/feeds/atom.xml");
   ("Mindy Preston", "http://www.somerandomidiot.com/atom.xml");
-  ("Amir Chaudhry", "http://amirchaudhry.com/rss.xml");
+  ("Amir Chaudhry", "http://feeds.feedburner.com/amirmc");
   ("My Little Garden of Code", "https://philtomson.github.io/atom.xml");
   ("Thomas Leonard", "http://roscidus.com/blog/atom.xml");
 ]
