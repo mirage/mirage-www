059ce7b29577b17067f670a3c778ff020fa9f87b
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-06-12 17:28:06 +0100

    Simplify the dispatch function

--- src/dispatch.ml
+++ src/dispatch.ml
@@ -1,113 +1,102 @@
-open Lwt
 open Printf
 
+open Lwt.Infix
+
 module Main
-  (C: V1_LWT.CONSOLE) (FS: V1_LWT.KV_RO) (TMPL: V1_LWT.KV_RO)
-  (S: Cohttp_lwt.Server)
-  = struct
+    (C: V1_LWT.CONSOLE) (FS: V1_LWT.KV_RO) (TMPL: V1_LWT.KV_RO)
+    (S: Cohttp_lwt.Server)
+= struct
 
-    let start c fs tmpl http =
+  let log c fmt = Printf.kprintf (C.log c) fmt
+  let err fmt = Printf.kprintf (fun f -> raise (Failure f)) fmt
+  let err_not_found name = err "%s not found" name
 
-      let read_tmpl name =
-        TMPL.size tmpl name
-        >>= function
-        | `Error (TMPL.Unknown_key _) -> fail (Failure ("read " ^ name))
-        | `Ok size ->
-          TMPL.read tmpl name 0 (Int64.to_int size)
-          >>= function
-          | `Error (TMPL.Unknown_key _) -> fail (Failure ("read " ^ name))
-          | `Ok bufs -> return (Cstruct.copyv bufs)
-      in
+  let read_tmpl tmpl name =
+    TMPL.size tmpl name >>= function
+    | `Error (TMPL.Unknown_key _) -> err_not_found name
+    | `Ok size ->
+      TMPL.read tmpl name 0 (Int64.to_int size) >>= function
+      | `Error (TMPL.Unknown_key _) -> err_not_found name
+      | `Ok bufs -> Lwt.return (Cstruct.copyv bufs)
 
-      let read_fs name =
-        FS.size fs name
-        >>= function
-        | `Error (FS.Unknown_key _) -> fail (Failure ("read " ^ name))
-        | `Ok size ->
-          FS.read fs name 0 (Int64.to_int size)
-          >>= function
-          | `Error (FS.Unknown_key _) -> fail (Failure ("read " ^ name))
-          | `Ok bufs -> return (Cstruct.copyv bufs)
-      in
+  let read_fs fs name =
+    FS.size fs name >>= function
+    | `Error (FS.Unknown_key _) -> err_not_found name
+    | `Ok size ->
+      FS.read fs name 0 (Int64.to_int size) >>= function
+      | `Error (FS.Unknown_key _) -> err_not_found name
+      | `Ok bufs -> Lwt.return (Cstruct.copyv bufs)
 
-      let respond_ok ?(headers=[]) body =
-        body >>= fun body ->
-        let status = `OK in
-        let headers = Cohttp.Header.of_list headers in
-        S.respond_string ~headers ~status ~body ()
-      in
-      let read_entry =
-        (fun name -> read_tmpl name >|= Cow.Markdown.of_string)
-      in
-      let blog_feed = Site_config.blog (fun n -> read_entry ("/blog/"^n)) in
-      let wiki_feed = Site_config.wiki (fun n -> read_entry ("/wiki/"^n)) in
-      let updates_feed = Site_config.updates read_entry in
-      let links_feed = Site_config.links read_entry in
+  let read_entry tmpl name = read_tmpl tmpl name >|= Cow.Markdown.of_string
 
-      let updates_feeds = [
-        `Blog (blog_feed, Data.Blog.entries);
-        `Wiki (wiki_feed, Data.Wiki.entries);
-      ] in
+  let respond_ok ?(headers=[]) body =
+    body >>= fun body ->
+    let status = `OK in
+    let headers = Cohttp.Header.of_list headers in
+    S.respond_string ~headers ~status ~body ()
 
-      lwt blog_dispatch = Blog.dispatch blog_feed Data.Blog.entries in
-      lwt wiki_dispatch = Wiki.dispatch wiki_feed Data.Wiki.entries in
-      lwt releases_dispatch = Pages.Releases.dispatch read_tmpl in
-      lwt links_dispatch =
-        Pages.Links.dispatch links_feed Data.Links.entries
-      in
-      lwt updates_dispatch =
-        Pages.Index.dispatch ~feed:updates_feed ~feeds:updates_feeds
-      in
+  let not_found page = Lwt.return (`Not_found page)
+  let html h = Lwt.return (`Html h)
+  let page p path = p >|= fun f -> (`Page (f path))
+  let redirect r = Lwt.return (`Redirect r)
 
-      (* dispatch non-file URLs *)
-      let dispatcher = function
-        | [] | [""] | ["index.html"] ->
-          return (`Html (Pages.Index.t ~feeds:updates_feeds read_tmpl))
+  let blog_feed tmpl = Site_config.blog (fun n -> read_entry tmpl ("/blog/"^n))
+  let wiki_feed tmpl = Site_config.wiki (fun n -> read_entry tmpl ("/wiki/"^n))
+  let updates_feed tmpl = Site_config.updates (read_entry tmpl)
+  let links_feed tmpl = Site_config.links (read_entry tmpl)
 
-        | ["stats"; "gc"] ->
-          let gc = return (Cow.Html.to_string (Stats.page ())) in
-          return (`Html gc)
+  let updates_feeds tmpl = [
+    `Blog (blog_feed tmpl, Data.Blog.entries);
+    `Wiki (wiki_feed tmpl, Data.Wiki.entries);
+  ]
 
-        | ["about"]
-        | ["community"] ->
-          return (`Html (Pages.About.t read_tmpl))
+  let blog_dispatch tmpl = Blog.dispatch (blog_feed tmpl) Data.Blog.entries
+  let wiki_dispatch tmpl = Wiki.dispatch (wiki_feed tmpl) Data.Wiki.entries
+  let releases_dispatch tmpl = Pages.Releases.dispatch (read_tmpl tmpl)
+  let links_dispatch tmpl = Pages.Links.dispatch (links_feed tmpl) Data.Links.entries
+  let updates_dispatch tmpl =
+    Pages.Index.dispatch ~feed:(updates_feed tmpl) ~feeds:(updates_feeds tmpl)
 
-        | "releases" :: tl -> return (`Page (releases_dispatch tl))
-        | "blog"     :: tl -> return (`Page (blog_dispatch tl))
-        | "links"    :: tl -> return (links_dispatch tl)
-        | "updates"  :: tl -> return (`Page (updates_dispatch tl))
+  let stats () = html (Lwt.return (Cow.Html.to_string (Stats.page ())))
+  let redirect () = redirect "../wiki#Weeklycallsandreleasenotes"
+  let index tmpl = html (Pages.Index.t ~feeds:(updates_feeds tmpl) (read_tmpl tmpl))
+  let about tmpl = html (Pages.About.t (read_tmpl tmpl))
 
-        | ("wiki" | "docs") :: "weekly" :: tl -> return (`Redirect "../wiki#Weeklycallsandreleasenotes")
-        | "docs" :: tl
-        | "wiki" :: tl -> return (`Page (wiki_dispatch tl))
+  let asset c fs path =
+    let path = String.concat "/" path in
+    let asset path = Lwt.return (`Asset (read_fs fs path)) in
+    Lwt.catch (fun () -> asset path) (fun e  -> not_found path)
 
-        | segments ->
-          let path = String.concat "/" segments in
-          try_lwt
-            lwt body = read_fs path in
-            return (`Asset (return body))
-          with exn ->
-            return (`Not_found path)
-      in
+  (* dispatch non-file URLs *)
+  let dispatcher c fs tmpl = function
+    | [] | [""] | ["index.html"] -> index tmpl
+    | ["stats"; "gc"] -> stats ()
+    | ["about"] | ["community"] -> about tmpl
+    | "releases" :: tl -> page (releases_dispatch tmpl) tl
+    | "blog"     :: tl -> page (blog_dispatch tmpl) tl
+    | "links"    :: tl -> links_dispatch tmpl >|= fun f -> f tl
+    | "updates"  :: tl -> page (updates_dispatch tmpl) tl
+    | ("wiki" | "docs") :: "weekly" :: tl -> redirect ()
+    | "docs" :: tl | "wiki" :: tl -> page (wiki_dispatch tmpl) tl
+    | path -> asset c fs path
 
-      (* HTTP callback *)
-      let callback conn_id request body =
-        let uri = Cohttp.Request.uri request in
-        let io = { Cowabloga.Dispatch.
-                   log = (fun ~msg -> C.log c msg);
-                   ok = respond_ok;
-                   notfound = (fun ~uri -> S.respond_not_found ~uri ());
-                   redirect = (fun ~uri -> S.respond_redirect ~uri ());
-                 }
-        in
-        Cowabloga.Dispatch.f io dispatcher uri
-      in
-      let conn_closed (_,conn_id) =
-        let cid = Cohttp.Connection.to_string conn_id in
-        C.log c (Printf.sprintf "conn %s closed" cid)
-      in
+  let start c fs tmpl http =
+    (* HTTP callback *)
+    let callback conn_id request body =
+      let uri = Cohttp.Request.uri request in
+      let io = {
+        Cowabloga.Dispatch.log = (fun ~msg -> C.log c msg);
+        ok = respond_ok;
+        notfound = (fun ~uri -> S.respond_not_found ~uri ());
+        redirect = (fun ~uri -> S.respond_redirect ~uri ());
+      } in
+      Cowabloga.Dispatch.f io (dispatcher c fs tmpl) uri
+    in
+    let conn_closed (_,conn_id) =
+      let cid = Cohttp.Connection.to_string conn_id in
+      C.log c (Printf.sprintf "conn %s closed" cid)
+    in
+    Stats.start OS.Time.sleep;
+    http (`TCP 80) (S.make ~callback ~conn_closed ())
 
-      Stats.start OS.Time.sleep;
-      http (`TCP 80) (S.make ~callback ~conn_closed ())
-
-  end
+end
