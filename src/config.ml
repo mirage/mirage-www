8151857abab4e93dc3588dec717edb08fe853cf2
Drup <drupyog@zoho.com>
2016-02-27 15:15:04 +0100

    Use the mirage combinators.

--- src/config.ml
+++ src/config.ml
@@ -68,44 +68,35 @@ let get ~default name fn =
     env_info "%s => not set." name;
     default
 
-let fs = get "FS" ~default:`Crunch fat_of_env
-let deploy = get "DEPLOY" ~default:false bool_of_env
-let net = get  "NET" ~default:`Direct socket_of_env
-let dhcp = get "DHCP" ~default:false bool_of_env
-let tls = get "TLS" ~default:false bool_of_env
+let tls_key =
+  let doc = Key.Arg.info
+      ~doc:"Enable serving the website over https."
+      ~env:"TLS" ["tls"]
+  in
+  Key.(create "tls" Arg.(opt ~stage:`Configure bool false doc))
+
+let pr_key =
+  let doc = Key.Arg.info
+      ~doc:"Configuration for running inside a travis PR."
+      ~env:"TRAVIS_PULL_REQUEST" ["pr"]
+  in
+  Key.(create "pr" Arg.(flag ~stage:`Configure doc))
+
 let host = get "HOST" ~default:None opt_string_of_env
 let redirect = get "REDIRECT" ~default:None opt_string_of_env
 let image = get "XENIMG" ~default:"www" string_of_env
 
-let mkfs fs path =
-  let fat_of_files dir = kv_ro_of_fs (fat_of_files ~dir ()) in
-  match fs, get_mode () with
-  | `Fat   , _    -> fat_of_files path
-  | `Archive, _   -> archive_of_files ~dir:path ()
-  | `Crunch, `Xen -> crunch path
-  | `Crunch, _    -> direct_kv_ro path
+let filesfs = generic_kv_ro ~group:"file" "../files"
+let tmplfs = generic_kv_ro ~group:"tmpl" "../tmpl"
 
-let filesfs = mkfs fs "../files"
-let tmplfs = mkfs fs "../tmpl"
-let cons0 = default_console
+(* If we are running inside a PR in Travis CI,
+   we don't try to get the server certificates. *)
+let secrets =
+  if_impl (Key.value pr_key)
+    (crunch "../src")
+    (generic_kv_ro ~group:"secret" "../tls")
 
-let stack = match deploy with
-  | true ->
-    let staticip =
-      let address = get_exn "IP" Ipaddr.V4.of_string_exn in
-      let netmask = get_exn "NETMASK" Ipaddr.V4.of_string_exn in
-      let gateways = get_exn "GATEWAYS" ips_of_env in
-      { address; netmask; gateways }
-    in
-    direct_stackv4_with_static_ipv4 cons0 tap0 staticip
-  | false ->
-    match net, dhcp with
-    | `Direct, false -> direct_stackv4_with_default_ipv4 cons0 tap0
-    | `Direct, true  -> direct_stackv4_with_dhcp cons0 tap0
-    | `Socket, _     -> socket_stackv4 cons0 [Ipaddr.V4.any]
-
-let libraries = [ "cow.syntax"; "cowabloga"; "rrd" ]
-let packages  = [ "cow"; "cowabloga"; "xapi-rrd"; "c3" ]
+let stack = generic_stackv4 default_console tap0
 
 let sp = Printf.sprintf
 
@@ -117,40 +108,34 @@ let config =
 let main = sp "Make(%s)" config
 
 let http =
-  foreign ~libraries ~packages ("Dispatch." ^ main)
+  foreign ("Dispatch." ^ main)
     (console @-> kv_ro @-> kv_ro @-> http @-> clock @-> job)
 
 let https =
-  let libraries = "tls" :: "tls.mirage" :: "mirage-http" :: libraries in
-  let packages = "tls" :: "tls" :: "mirage-http" :: packages in
+  let libraries = [ "tls"; "tls.mirage"; "mirage-http" ] in
+  let packages = ["tls"; "tls"; "mirage-http"] in
   foreign ~libraries ~packages ("Dispatch_tls." ^ main)
     ~deps:[abstract nocrypto]
     (console @-> kv_ro @-> kv_ro @-> stackv4 @-> kv_ro @-> clock @-> job)
 
-let err fmt = Printf.ksprintf (fun msg ->
-    Printf.eprintf "\n\027[31m[ERROR]\027[m     %s, stopping.\n%!" msg;
-    exit 1
-  ) fmt
+
+let dispatch = if_impl (Key.value tls_key)
+    (** With tls *)
+    begin
+      let clock = default_clock in
+      https $ default_console $ filesfs $ tmplfs $ stack $ secrets $ clock
+    end
+    (** Without tls *)
+    begin
+      let server = http_server (conduit_direct stack) in
+      let clock = default_clock in
+      http  $ default_console $ filesfs $ tmplfs $ server $ clock
+    end
+
+let libraries = [ "cow.syntax"; "cowabloga"; "rrd" ]
+let packages  = [ "cow"; "cowabloga"; "xapi-rrd"; "c3" ]
 
 let () =
   let tracing = None in
   (* let tracing = mprof_trace ~size:10000 () in *)
-  register ?tracing image [ match tls with
-      | false ->
-        let server = http_server (conduit_direct stack) in
-        let clock = default_clock in
-        http  $ default_console $ filesfs $ tmplfs $ server $ clock
-      | true ->
-        let pr = get ~default:None "TRAVIS_PULL_REQUEST" opt_string_of_env in
-        let secrets = get "SECRETS" ~default:`Crunch fat_of_env in
-        let clock = default_clock in
-        let tls =
-          match pr with
-          | None | Some "false" -> mkfs secrets "../tls"
-          | _ ->
-            (* we are running inside a PR in Travis CI. Don't try to
-               get the server certificates. *)
-            mkfs `Crunch "../src"
-        in
-        https $ default_console $ filesfs $ tmplfs $ stack $ tls $ clock
-    ]
+  register ?tracing ~libraries ~packages image [ dispatch ]
