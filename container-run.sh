abc95b0ac1f2db214ce6ef80e7dddcb16940ce3c
Anil Madhavapeddy <anil@recoil.org>
2016-06-01 18:33:18 +0100

    Add Dockerfile to make it easier to run the Unix version on D4Mac/Win

---
+++ container-run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Convenience script to run inside a Docker continer.
+# 
+#    docker build -t mirage-www
+#    docker run -Pd mirage-www
+#    docker ps
+
+export OPAMJOBS=2
+export OPAMYES=1
+eval `opam config env`
+opam depext -ui mirage
+cd src
+mirage configure --unix --net=socket --http-port=8080
+make
