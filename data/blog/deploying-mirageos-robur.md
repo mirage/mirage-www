d70a17edc17f47380a5ff38985f7c781ff4b4b3a
Hannes Mehnert <hannes@mehnert.org>
2022-03-08 12:35:27 +0100

    blog post about deploying mirageos unikernels

---
+++ data/blog/deploying-mirageos-robur.md
@@ -0,0 +1,60 @@
+---
+updated: 2022-03-08 13:00
+authors:
+- name: Rand
+  uri: https://r7p5.earth
+- name: Reynir Björnsson
+  uri: https://reynir.dk
+- name: Hannes Mehnert
+  uri: https://hannes.robur.coop
+subject: Deploying MirageOS unikernels using binaries
+permalink: deploying-mirageos-robur
+---
+
+We are pleased to announce that the EU [NGI Pointer](https://pointer.ngi.eu) funding received by [robur](https://robur.coop) in 2021 lead to improved operations for MirageOS unikernels.
+
+Our main achievement are [reproducible binary builds](https://builds.robur.coop) of opam packages, including MirageOS unikernels and system packages. The infrastructure behind it, [orb](https://github.com/roburio/orb), [builder](https://github.com/roburio/builder), [builder-web](https://github.com/roburio/builder-web) is itself reproducible and delivered as packages by [builds.robur.coop](https://builds.robur.coop).
+
+The documentation how to get started [installing MirageOS unikernels and albatross from packages](https://robur.coop/Projects/Reproducible_builds) is available online, further documentation on [monitoring](https://hannes.robur.coop/Posts/Monitoring) is available as well.
+
+The funding proposal covered the parts (as outlined in [an earlier post from 2020](https://hannes.robur.coop/Posts/NGI)):
+* reproducible binary releases of MirageOS unikernels,
+* monitoring (and other devops features: profiling) and integration into existing infrastructure,
+* and further documentation and advertisement.
+
+We [announced the web interface earlier](https://discuss.ocaml.org/t/ann-robur-reproducible-builds/8827) and also [posted about deployment](https://hannes.robur.coop/Posts/Deploy) possibilities.
+
+At the heart of our infrastructure is [builder-web](https://github.com/roburio/builder-web), a database that receives binary builds and provides a web interface and binary package repositories ([apt.robur.coop](https://apt.robur.coop) and [pkg.robur.coop](https://pkg.robur.coop)). Reynir discusses the design and implementation of [builder-web](https://github.com/roburio/builder-web) in [his blogpost](https://reyn.ir/posts/2022-03-08-builder-web.html).
+
+There we [visualize](https://builds.robur.coop/job/tlstunnel/build/7f0afdeb-0a52-4de1-b96f-00f654ce9249/) the opam dependencies of an opam package:
+
+<iframe src="../graphics/tlstunnel-deps.html" title="Opam dependencies" style="
+      width: 45em;
+      height: 45.4em;
+      max-width: 100%;
+      max-height: 49vw;
+      min-width: 38em;
+      min-height: 40em;
+     "></iframe>
+
+We also visualize the contributing modules and their sizes to the binary:
+
+<iframe src="../graphics/tlstunnel-treemap.html" title="Binary dissection" style="
+      width: 46em;
+      height: 48.4em;
+      max-width: 100%;
+      max-height: 52vw;
+      min-width: 38em;
+      min-height: 43em;
+    "></iframe>
+
+[![Tlstunnel dependencies](https://data.robur.coop/tlstunnel-deps.svg "Tlstunnel deps")](https://data.robur.coop/tlstunnel-deps.svg)
+
+We also visualize the contributing modules and their sizes to the binary:
+
+[![Tlstunnel size top](https://data.robur.coop/tlstunnel-treemap-top.svg "Tlstunnel treemap top")](https://data.robur.coop/tlstunnel-treemap-top.svg)
+[![Tlstunnel size](https://data.robur.coop/tlstunnel-treemap.svg "Tlstunnel treemap")](https://data.robur.coop/tlstunnel-treemap.svg)
+
+Rand wrote a more in-depth explanation about the [visualizations](https://builds.robur.coop/job/tlstunnel/build/7f0afdeb-0a52-4de1-b96f-00f654ce9249/) [on his blog](https://r7p5.earth/blog/2022-3-7/Builder-web%20visualizations%20at%20Robur).
+
+If you've comments or are interested in deploying MirageOS unikernels at your organization, [get in touch with us](https://robur.coop/Contact).
--- data/mirageio_data.ml
+++ data/mirageio_data.ml
@@ -14,6 +14,68 @@ module Blog = struct
   let all =
     [
       {
+        updated = {js|2022-03-08 13:00|js};
+        authors =
+          [
+            {
+              name = {js|rand|js};
+              uri = Some {js|https://r7p5.earth|js};
+              email = None;
+            };
+            {
+              name = {js|Reynir Björnsson|js};
+              uri = Some {js|https://reynir.dk|js};
+              email = None;
+            };
+            {
+              name = {js|Hannes Mehnert|js};
+              uri = Some {js|https://hannes.robur.coop|js};
+              email = None;
+            };
+          ];
+        subject = {js|Deploying MirageOS using binaries|js};
+        permalink = {js|deploying-mirageos-robur|js};
+        body =
+          {js|<p>We are pleased to announce that the EU <a href="https://pointer.ngi.eu">NGI Pointer</a> funding received by <a href="https://robur.coop">robur</a> in 2021 lead to improved operations for MirageOS unikernels.</p>
+<p>Our main achievement are <a href="https://builds.robur.coop">reproducible binary builds</a> of opam packages, including MirageOS unikernels and system packages. The infrastructure behind it, <a href="https://github.com/roburio/orb">orb</a>, <a href="https://github.com/roburio/builder">builder</a>, <a href="https://github.com/roburio/builder-web">builder-web</a> is itself reproducible and delivered as packages by <a href="https://builds.robur.coop">builds.robur.coop</a>.</p>
+<p>The documentation how to get started <a href="https://robur.coop/Projects/Reproducible_builds">installing MirageOS unikernels and albatross from packages</a> is available online, further documentation on <a href="https://hannes.robur.coop/Posts/Monitoring">monitoring</a> is available as well.</p>
+<p>The funding proposal covered the parts (as outlined in <a href="https://hannes.robur.coop/Posts/NGI">an earlier post from 2020</a>):</p>
+<ul>
+<li>reproducible binary releases of MirageOS unikernels,
+</li>
+<li>monitoring (and other devops features: profiling) and integration into existing infrastructure,
+</li>
+<li>and further documentation and advertisement.
+</li>
+</ul>
+<p>We <a href="https://discuss.ocaml.org/t/ann-robur-reproducible-builds/8827">announced the web interface earlier</a> and also <a href="https://hannes.robur.coop/Posts/Deploy">posted about deployment</a> possibilities.</p>
+<p>At the heart of our infrastructure is <a href="https://github.com/roburio/builder-web">builder-web</a>, a database that receives binary builds and provides a web interface and binary package repositories (<a href="https://apt.robur.coop">apt.robur.coop</a> and <a href="https://pkg.robur.coop">pkg.robur.coop</a>). Reynir discusses the design and implementation of <a href="https://github.com/roburio/builder-web">builder-web</a> in <a href="https://reyn.ir/posts/2022-03-08-builder-web.html">his blogpost</a>.</p>
+<p>There we <a href="https://builds.robur.coop/job/tlstunnel/build/7f0afdeb-0a52-4de1-b96f-00f654ce9249/">visualize</a> the opam dependencies of an opam package:</p>
+<iframe src="../graphics/tlstunnel-deps.html" title="Opam dependencies" style="
+      width: 45em;
+      height: 45.4em;
+      max-width: 100%;
+      max-height: 49vw;
+      min-width: 38em;
+      min-height: 40em;
+     "></iframe>
+<p>We also visualize the contributing modules and their sizes to the binary:</p>
+<iframe src="../graphics/tlstunnel-treemap.html" title="Binary dissection" style="
+      width: 46em;
+      height: 48.4em;
+      max-width: 100%;
+      max-height: 52vw;
+      min-width: 38em;
+      min-height: 43em;
+    "></iframe>
+<p><a href="https://data.robur.coop/tlstunnel-deps.svg"><img src="https://data.robur.coop/tlstunnel-deps.svg" alt="Tlstunnel dependencies" title="Tlstunnel deps" /></a></p>
+<p>We also visualize the contributing modules and their sizes to the binary:</p>
+<p><a href="https://data.robur.coop/tlstunnel-treemap-top.svg"><img src="https://data.robur.coop/tlstunnel-treemap-top.svg" alt="Tlstunnel size top" title="Tlstunnel treemap top" /></a>
+<a href="https://data.robur.coop/tlstunnel-treemap.svg"><img src="https://data.robur.coop/tlstunnel-treemap.svg" alt="Tlstunnel size" title="Tlstunnel treemap" /></a></p>
+<p>Rand wrote a more in-depth explanation about the <a href="https://builds.robur.coop/job/tlstunnel/build/7f0afdeb-0a52-4de1-b96f-00f654ce9249/">visualizations</a> <a href="https://r7p5.earth/blog/2022-3-7/Builder-web%20visualizations%20at%20Robur">on his blog</a>.</p>
+<p>If you've comments or are interested in deploying MirageOS unikernels at your organization, <a href="https://robur.coop/Contact">get in touch with us</a>.</p>|js}
+      };
+      {
         updated = {js|2022-02-10 17:00|js};
         authors =
           [
