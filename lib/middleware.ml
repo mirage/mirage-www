5c5a51ecdf8ecf6837fa10efa2b9497596603d24
Lucas Pluvinage <lucas@tarides.com>
2023-01-04 17:26:28 +0100

    handle trailing slashes (#788)

--- lib/middleware.ml
+++ lib/middleware.ml
@@ -26,4 +26,14 @@ struct
           ~headers:(Dream.all_headers response)
           (Dream.status response)
     | _ -> handler request
+
+  let no_trailing_slash next_handler request =
+    let target = Dream.target request in
+    match target with
+    | "/" -> next_handler request
+    | _ ->
+        let length = String.length target in
+        if target.[length - 1] = '/' then
+          Dream.redirect request (String.sub target 0 (length - 1))
+        else next_handler request
 end
